dca151a8a3bdc16defdf871acaa5859e8a0677eb
Paul Colomiets <paul@colomiets.name>
2016-10-14 01:43:22 +0300

    First prototype works

---
+++ examples/send_myself.rs
@@ -0,0 +1,37 @@
+extern crate futures;
+extern crate tokio_core;
+extern crate tk_sendfile;
+
+use std::net::SocketAddr;
+use std::env;
+
+use futures::Future;
+use futures::stream::Stream;
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::Core;
+use tk_sendfile::DiskPool;
+
+
+fn main() {
+    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = addr.parse::<SocketAddr>().unwrap();
+    let disk_pool = DiskPool::new();
+
+    let mut lp = Core::new().unwrap();
+    let handle = lp.handle();
+    let socket = TcpListener::bind(&addr, &handle).unwrap();
+    println!("Listening on: {}", addr);
+
+    let done = socket.incoming().for_each(|(socket, _addr)| {
+        handle.spawn(
+            disk_pool.send("./examples/send_myself.rs", socket)
+            .then(|result| {
+                println!("Result {:?}", result);
+                Ok(())
+            })
+        );
+        Ok(())
+    });
+
+    lp.run(done).unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
+//! A thread pool that can process file requests and send data to the socket
+//! with zero copy (using sendfile).
+//!
+//! Use `DiskPool` structure to request file operations.
+#![deny(missing_docs)]
+
+extern crate libc;
+extern crate futures;
+extern crate tokio_core;
+extern crate futures_cpupool;
+
 use std::io;
 use std::fs::File;
+use std::sync::Arc;
 use std::path::PathBuf;
 use std::os::unix::io::AsRawFd;
 
+use libc::sendfile;
+use futures_cpupool::CpuPool;
+
+/// A reference to a thread pool for disk operations
+#[derive(Clone)]
+pub struct DiskPool(Arc<Inner>);
 
 /// This trait represents anything that can open the file
 ///
@@ -28,7 +46,7 @@ pub trait FileOpener: Send + 'static {
     /// Open the file
     ///
     /// This function is called in disk thread
-    fn open(&mut self) -> Result<&AsRawFd, io::Error>;
+    fn open(&mut self) -> Result<(&AsRawFd, u64), io::Error>;
 }
 
 /// Trait that represents something that can be converted into a file
@@ -39,16 +57,41 @@ pub trait FileOpener: Send + 'static {
 ///
 /// Note unlike methods in FileOpener itself this trait is executed in
 /// caller thread, **not** in disk thread.
-pub trait IntoFileOpener {
-    type Opener;
+pub trait IntoFileOpener: Send {
+    /// The final type returned after conversion
+    type Opener: FileOpener + Send + 'static;
+    /// Convert the type into a file opener
     fn into_file_opener(self) -> Self::Opener;
 }
 
 /// File opener implementation that opens specified file path directly
 #[derive(Debug)]
-pub struct PathOpener(PathBuf, Option<File>);
+pub struct PathOpener(PathBuf, Option<(File, u64)>);
 
-impl<T: Into<PathBuf>> IntoFileOpener for T {
+/// A trait that represents anything that file can be sent to
+///
+/// The trait is implemented for TcpStream right away but you might want
+/// to implement your own thing, for example to prepend the data with file
+/// length header
+pub trait Destination: Send {
+    /// Write data to the file
+    fn write(&mut self, file: &mut Sendfile) -> Result<usize, io::Error>;
+}
+
+/// A structure that tracks progress of sending a file
+pub struct Sendfile {
+    file: Box<FileOpener>,
+    offset: u64,
+}
+
+/// Future that is returned from `DiskPool::send`
+type SendfileFuture<D> = futures_cpupool::CpuFuture<D, io::Error>;
+
+struct Inner {
+    pool: CpuPool,
+}
+
+impl<T: Into<PathBuf> + Send> IntoFileOpener for T {
     type Opener = PathOpener;
     fn into_file_opener(self) -> PathOpener {
         PathOpener(self.into(), None)
@@ -56,10 +99,64 @@ impl<T: Into<PathBuf>> IntoFileOpener for T {
 }
 
 impl FileOpener for PathOpener {
-    fn open(&mut self) -> Result<&AsRawFd, io::Error> {
+    fn open(&mut self) -> Result<(&AsRawFd, u64), io::Error> {
         if self.1.is_none() {
-            self.1 = Some(try!(File::open(&self.0)));
+            let file = try!(File::open(&self.0));
+            let meta = try!(file.metadata());
+            self.1 = Some((file, meta.len()));
         }
-        Ok(self.1.as_ref().unwrap())
+        Ok(self.1.as_ref().map(|&(ref f, s)| (f as &AsRawFd, s)).unwrap())
     }
+}
+
+impl DiskPool {
+    /// Create a disk pool with default configuration
+    pub fn new() -> DiskPool {
+        DiskPool(Arc::new(Inner {
+            pool: CpuPool::new(20),
+        }))
+    }
+    /// Start a file send operation
+    pub fn send<F, D>(&self, file: F, destination: D)
+        -> SendfileFuture<D>
+        where F: IntoFileOpener + Send + Sized + 'static,
+              D: Destination + Send + Sized + 'static,
+    {
+        let file = Sendfile {
+            file: Box::new(file.into_file_opener()),
+            offset: 0,
+        };
+        self.0.pool.spawn_fn(move || {
+            read_file(file, destination)
+        })
+    }
+}
+
+impl Destination for tokio_core::net::TcpStream {
+    fn write(&mut self, file: &mut Sendfile) -> Result<usize, io::Error> {
+        let (file_ref, size) = try!(file.file.open());
+        let mut offset = file.offset as i64;
+        let rc = unsafe {
+            sendfile(self.as_raw_fd(), file_ref.as_raw_fd(),
+                     &mut offset,
+                     size.saturating_sub(file.offset) as usize)
+        };
+        if rc < 0 {
+            Err(io::Error::last_os_error())
+        } else if rc == 0 {
+            Err(io::Error::new(io::ErrorKind::WriteZero,
+                           "connection closed while sending file"))
+        } else {
+            Ok(rc as usize)
+        }
+    }
+}
+
+fn read_file<D>(mut file: Sendfile, mut dest: D)
+    -> Result<D, io::Error>
+    where D: Destination
+{
+    dest.write(&mut file)
+    .map(|bytes_sent| file.offset += bytes_sent as u64)
+    .map(|()| dest)
 }
